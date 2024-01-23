e4c82eb36b8c14c1c1abf5d77ba71138edd6b144
MuzzamilBukhari <muhammadmuzzamilbukhari@gmail.com>
2024-01-23 17:43:36 +0500

    Make a binary search func

--- binarySearch.cpp
+++ binarySearch.cpp
@@ -1,18 +1,14 @@
-// "Embarking on a journey into Data Structures and Algorithms in C++! 🚀 Today marked a milestone as I conquered the basics with Linear Search and Binary Search Algorithms. Excited for the challenges and learning ahead! 💻🔍 #DSA #Cplusplus #LearningJourney #AlgorithmMastery"
-
 #include<iostream>
 using namespace std;
-int main (){
-    int n = 5;
-    int arr[5] = {32, 45, 49, 53, 72};
-    int toSearch = 54;
+
+// Function to Search the number using Binary search
+int binarySearch (int arr[], int n, int toSearch){
     int si = 0;
     int li = n-1;
     while (si <= li){
         int mi = (si + li) / 2;
         if (arr[mi] == toSearch){
-            cout<<"Found at position "<<mi<<endl;
-            break;
+            return mi;
         }
         if (arr[mi] > toSearch){
             li = mi - 1;
@@ -21,6 +17,22 @@ int main (){
         }
     };
     if (si > li){
-        cout<<"Not found";
+        return -1;
     }
+}
+int main (){
+    int n;
+    cout<<"Enter the size of an array : ";
+    cin >> n;
+    int arr[n];
+    int toSearch;
+    cout<< "Enter array elements"<<endl;
+    for (int i = 0;i<n;i++){
+      cin >> arr[i];
+    }
+    cout<<"Enter the number you want to search in this array : ";
+    cin >> toSearch;
+    int pos = binarySearch(arr, n, toSearch);
+    pos >= 0 ? cout<<"Found at position "<<pos : cout<<"Not found"; 
+    
 }
